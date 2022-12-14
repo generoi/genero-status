e666aaafce71eb91321f701bf145c116f2da5a68
Oskar Schöldström <m@oxy.fi>
2022-12-14 14:15:21 -0300

    Update script

--- genero-status.php
+++ genero-status.php
@@ -1,149 +1,77 @@
 <?php
 /**
  * genero-status.php
- * =================
- *
- * A script to output general health status of a Drupal or Wordpress website.
- *
- * This script is used in our Website overview spreadsheet to monitor the
- * status of our websites.
- *
- * Formula:
- * -------
- * =IF(
- *   AND(D12 = "x", G12 = "x"), -- The website is active and live
- *   IMPORTXML(
- *     CONCAT(CONCAT("http://", A12), "/genero-status.php?key=fAme9hS3Kduggk3F"), -- http://<domain>/genero.status.php?key=<key>
- *     "/status/version" -- xpath selector for which field to fetch
- *   ),
- *   "" -- empty output if the website isnt active or isnt live yet.
- * )
- *
- * Supported versions:
- * - Drupal 6, 7
- * - Wordpress 3.4 (at least)
  */
-header('Content-Type: application/xml; charset=utf-8');
 
-define('GENERO_STATUS_VERSION', '0.0.8');
-define('GENERO_DEBUG', FALSE);
-define('GENERO_KEY', 'fAme9hS3Kduggk3F');
-define('GENERO_DRUPAL', 'Drupal');
-define('GENERO_WORDPRESS', 'Wordpress');
-define('GENERO_UNKNOWN', 'unknown');
-define('GENERO_SCRIPT_ROOT', dirname($_SERVER['SCRIPT_FILENAME']));
+header('Cache-Control: max-age=0, no-cache, must-revalidate');
+header('Pragma: no-cache');
+header('Expires: Wed, 11 Jan 1984 05:00:00 GMT');
+header('Content-Type: text/plain; charset=utf-8');
 
-if (GENERO_DEBUG) {
-  error_reporting(E_ALL);
-  ini_set('display_errors', 1);
-} else {
-  error_reporting(0);
-}
-
-foreach (array('wp', 'web', 'app', 'wordpress') as $subdir) {
-  // If there's an index file in any of these sub directories, use that as the
-  // root instead.
-  if (is_file(GENERO_SCRIPT_ROOT . '/' . $subdir . '/index.php')) {
-    define('GENERO_ENV_ROOT', GENERO_SCRIPT_ROOT . '/' . $subdir);
-    break;
-  }
-}
-if (!defined('GENERO_ENV_ROOT')) {
-  // Fallback to the directory where this script is located.
-  define('GENERO_ENV_ROOT', GENERO_SCRIPT_ROOT);
-}
-
-if (empty($_GET['key']) || $_GET['key'] != GENERO_KEY) {
+function exitWithError(string $message): void
+{
+  http_response_code(503);
+  echo $message . PHP_EOL;
   exit;
 }
 
-function genero_get_platform() {
-  if (file_exists(GENERO_ENV_ROOT . '/includes/bootstrap.inc')) {
-    return GENERO_DRUPAL;
+if (! is_readable($bootstrap = __DIR__ . '/wp-config.php')) {
+  exitWithError('Filesystem not found.');
+}
+
+// Minimally bootstrap WordPress
+define('WP_INSTALLING', TRUE);
+define('WP_SETUP_CONFIG', TRUE);
+define('SHORTINIT', TRUE);
+include_once $bootstrap;
+
+// Optionally authenticate the HTTP request
+if (defined('STATUS_API_KEY') && STATUS_API_KEY) {
+  $user = $_SERVER['PHP_AUTH_USER'] ?? null;
+  $pass = $_SERVER['PHP_AUTH_PW'] ?? null;
+  if (STATUS_API_KEY !== "$user:$pass") {
+    header('WWW-Authenticate: Basic realm="genero"');
+    http_response_code(401);
+    echo 'Not allowed' . PHP_EOL;
+    exit;
   }
-  if (file_exists(GENERO_ENV_ROOT . '/wp-includes/version.php')) {
-    return GENERO_WORDPRESS;
-  }
-  return GENERO_UNKNOWN;
 }
 
-function genero_drupal_variable_validate($variables) {
-  foreach ($variables as $variable_name) {
-    if (!variable_get($variable_name, 0)) {
-      return FALSE;
-    }
-  }
-  return TRUE;
+// Validate WordPress environment variable
+if (defined('WP_ENV') && WP_ENV !== 'production') {
+  exitWithError(sprintf('In "%s" mode', WP_ENV));
 }
 
-function genero_gather_wordpress_data() {
-  $data['version'] = $GLOBALS['wp_version'];
-  $data['maintenance'] = file_exists(GENERO_ENV_ROOT . '/.maintenance') ? 'on' : 'off';
-  $data['database'] = $GLOBALS['wpdb']->db_connect(FALSE) ? 'on' : 'off';
-  return $data;
+// Validate database connection
+if (! $wpdb->db_connect(false)) {
+  exitWithError('Database is down');
 }
 
-function genero_gather_drupal_data() {
-  $data['version'] = VERSION;
-  $data['maintenance'] = genero_drupal_variable_validate(array('maintenance_mode')) ? 'on' : 'off';
-  $data['caching'] = genero_drupal_variable_validate(array('block_cache', 'cache', 'preprocess_css', 'preprocess_js', 'page_compression')) ? 'on' : 'off';
-  try {
-    // Drupal 7
-    if (class_exists('Database')) {
-      Database::getConnection();
-    }
-    $data['database'] = 'on';
-  } catch (Exception $e) {
-    // @todo figure out how to prevent Drupal 6 from spitting out an error
-    // page.
-    $data['database'] = 'off';
-  }
-  return $data;
+/** @var WP_Object_Cache $wp_object_cache */
+global $wp_object_cache;
+
+// Validate redis-cache plugin can connect to redis
+if (method_exists($wp_object_cache, 'redis_status') && ! $wp_object_cache->redis_status()) {
+  exitWithError('Redis cant connect');
+}
+// Validate wp-redis plugin can connect to redis
+if (isset($wp_object_cache->is_redis_connected) && ! $wp_object_cache->is_redis_connected) {
+  exitWithError('Redis cant connect');
 }
 
-switch (genero_get_platform()) {
-  case GENERO_DRUPAL:
-    define('DRUPAL_ROOT', GENERO_ENV_ROOT);
-    include_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-    drupal_bootstrap(DRUPAL_BOOTSTRAP_DATABASE);
-    // Required by some caching backends
-    include_once DRUPAL_ROOT . '/includes/lock.inc';
-    // Version definition exists here on Drupal 6.
-    include_once DRUPAL_ROOT . '/modules/system/system.module';
-    $data = genero_gather_drupal_data();
-    break;
-  case GENERO_WORDPRESS:
-    // Disable caching.
-    define('WP_CACHE', FALSE);
-    // Disable the regular bootstrap process, there's lots of exit calls which
-    // we cant prevent.
-    define('WP_INSTALLING', TRUE);
-    // This will prevent a failed database connection from issuing an exit call.
-    define('WP_SETUP_CONFIG', TRUE);
-    // Bootstrap as little as possible.
-    define('SHORTINT', TRUE);
-    // Support bedrock as well as traditional structure
-    if (is_file(GENERO_ENV_ROOT . '/wp-config.php')) {
-      include_once GENERO_ENV_ROOT . '/wp-config.php';
-    } else {
-      include_once GENERO_SCRIPT_ROOT . '/wp-config.php';
-    }
-    $data = genero_gather_wordpress_data();
-    break;
-  default:
-    $data = array('version' => 'unknown', 'maintenance' => 'unknown', 'database' => 'unknown');
+// Validate there's no "noindex" tag on the frontpage
+$content = file_get_contents(WP_HOME);
+if (preg_match('~<meta[^>]+robots[^>]+noindex~', $content) !== 0) {
+  exitWithError('Meta robots has noindex');
 }
 
-$data['platform'] = genero_get_platform();
-$data['php_version'] = PHP_VERSION;
-$data['script_version'] = GENERO_STATUS_VERSION;
+// Validate there's no Disallow all in the robots.txt
+$content = file_get_contents(rtrim(WP_HOME, '/') . '/robots.txt');
+if (preg_match('~Disallow:\h*/(?:\R|$)~i', $content) !== 0) {
+  exitWithError('robots.txt disallow /');
+}
 
-// Really? short-tags are not disabled?
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-?>
-<status>
-<?php foreach ($data as $key => $value): ?>
-<<?php print $key; ?>><?php print $value; ?></<?php print $key; ?>>
-<?php endforeach; ?>
-</status>
-<?php exit; ?>
+// Everything passed.
+http_response_code(200);
+header('Content-Type: text/plain; charset=utf-8');
+echo 'OK' . PHP_EOL;
